ed68e6585a3c89fdb1f3d3a34451bea3376e6144
Mestery <mestery@pm.me>
2021-04-23 21:11:47 +0200

    refactor: gateway

---
+++ src/utils/events.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { EventEmitter, once as $once } from 'events';
+
+declare module 'events' {
+	// eslint-disable-next-line @typescript-eslint/no-shadow
+	class EventEmitter {
+		public static once(emitter: EventEmitter, event: string | symbol, options: { signal: any }): Promise<unknown[]>;
+	}
+}
+
+export function once(emitter: EventEmitter, name: string, signal: unknown): Promise<any> {
+	return $once(emitter, name, { signal }).then(([arg]) => arg);
+}
+
+export function rejectOnce(emitter: EventEmitter, name: string, signal: unknown): Promise<any> {
+	return once(emitter, name, signal).then(Promise.reject);
+}
