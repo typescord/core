3addb79714b03550747a559dc73d265bb2a03248
Brewal Derouet <brewal_derouet@hotmail.fr>
2021-04-24 17:04:59 +0200

    Update src/gateway/Events.ts
    Co-authored-by: Mestery <mestery@pm.me>

--- src/gateway/Events.ts
+++ src/gateway/Events.ts
@@ -2,6 +2,6 @@ export enum Events {
 	CLIENT_READY = 'ready',
 	WEBSOCKET_DISCONNECTING = 'websocketDisconnecting',
 	WEBSOCKET_ERROR = 'websocketError',
-	WEBSOCKET_READY = 'websocketReady',
+	GATEWAY_READY = 'gatewayReady',
 	GATEWAY_RECONNECTION = 'gatewayReconnection',
 }
