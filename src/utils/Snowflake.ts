028cef3cda45f987660d0eed5adffd8a05d90b9b
Brewal <brewal_derouet@hotmail.fr>
2021-05-11 13:39:20 +0200

    fix: snowflake deconstrut format

--- src/utils/Snowflake.ts
+++ src/utils/Snowflake.ts
@@ -3,11 +3,11 @@ export const DISCORD_EPOCH = 1_420_070_400_000n;
 export const SnowflakeRegex = /^\d{17,20}$/;
 
 interface Snowflake {
-	timestamp: bigint;
+	timestamp: number;
 	date: Date;
-	workerId: bigint;
-	processId: bigint;
-	increment: bigint;
+	workerId: number;
+	processId: number;
+	increment: number;
 }
 
 /**
@@ -19,13 +19,13 @@ interface Snowflake {
 export function deconstruct(input: string): Snowflake | undefined {
 	try {
 		const snowflake = BigInt(input);
-		const timestamp = (snowflake >> 22n) + DISCORD_EPOCH;
+		const timestamp = Number((snowflake >> 22n) + DISCORD_EPOCH);
 		return {
 			timestamp,
-			date: new Date(Number(timestamp)),
-			workerId: (snowflake & 0x3e_00_00n) >> 17n,
-			processId: (snowflake & 0x1_f0_00n) >> 12n,
-			increment: snowflake & 0xf_ffn,
+			date: new Date(timestamp),
+			workerId: Number((snowflake & 0x3e_00_00n) >> 17n),
+			processId: Number((snowflake & 0x1_f0_00n) >> 12n),
+			increment: Number(snowflake & 0xf_ffn),
 		};
 		// eslint-disable-next-line no-empty
 	} catch {}
