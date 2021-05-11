d64c642e9e292a0d5c267c57f2adbcb72ee11a2a
Brewal <brewal_derouet@hotmail.fr>
2021-05-11 13:50:14 +0200

    feat(Snowflake): add $raw property

--- src/utils/Snowflake.ts
+++ src/utils/Snowflake.ts
@@ -2,31 +2,37 @@ export const DISCORD_EPOCH = 1_420_070_400_000n;
 
 export const SnowflakeRegex = /^\d{17,20}$/;
 
-interface Snowflake {
-	timestamp: number;
+export interface Snowflake<T extends number | bigint = number> {
+	timestamp: T;
 	date: Date;
-	workerId: number;
-	processId: number;
-	increment: number;
+	workerId: T;
+	processId: T;
+	increment: T;
+	$raw: Omit<Snowflake<bigint>, 'date' | '$raw'>;
 }
 
 /**
- * Descontruct a snowflake. **This doesn't check the validity of the snowflake.**
- *
- * @param input A snowflake.
- * @returns The decontructed snowflake or `undefined` if invalid BigInt.
- */
+* Descontruct a snowflake. **This doesn't check the validity of the snowflake.**
+*
+* @param input A snowflake.
+* @returns The decontructed snowflake or `undefined` if invalid BigInt.
+*/
 export function deconstruct(input: string): Snowflake | undefined {
 	try {
-		const snowflake = BigInt(input);
-		const timestamp = Number((snowflake >> 22n) + DISCORD_EPOCH);
-		return {
-			timestamp,
-			date: new Date(timestamp),
-			workerId: Number((snowflake & 0x3e_00_00n) >> 17n),
-			processId: Number((snowflake & 0x1_f0_00n) >> 12n),
-			increment: Number(snowflake & 0xf_ffn),
-		};
-		// eslint-disable-next-line no-empty
+			const snowflake = BigInt(input);
+			const timestamp = (snowflake >> 22n) + DISCORD_EPOCH;
+			const workerId = (snowflake & 0x3e_00_00n) >> 17n;
+			const processId = (snowflake & 0x1_f0_00n) >> 12n;
+			const increment = snowflake & 0xf_ffn;
+
+			return {
+					timestamp: Number(timestamp),
+					date: new Date(Number(timestamp)),
+					workerId: Number(workerId),
+					processId: Number(processId),
+					increment: Number(increment),
+					$raw: { timestamp, workerId, processId, increment },
+			};
+			// eslint-disable-next-line no-empty
 	} catch {}
 }
