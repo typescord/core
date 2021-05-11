92adf7e0cdc451d1af0dbd0e421e05d0bc2e7ece
Mestery <mestery@pm.me>
2021-05-11 14:01:06 +0200

    style: prettier

--- src/utils/Snowflake.ts
+++ src/utils/Snowflake.ts
@@ -12,16 +12,16 @@ export interface Snowflake<T extends number | bigint = number> {
 }
 
 /**
-* Descontruct a snowflake. **This doesn't check the validity of the snowflake.**
-*
-* @param input A snowflake.
-* @returns The decontructed snowflake or `undefined` if invalid BigInt.
-*/
+ * Descontruct a snowflake. **This doesn't check the validity of the snowflake.**
+ *
+ * @param input A snowflake.
+ * @returns The decontructed snowflake or `undefined` if invalid BigInt.
+ */
 export function deconstruct(input: string): Snowflake | undefined {
 	try {
 		const snowflake = BigInt(input);
 		const timestamp = (snowflake >> 22n) + DISCORD_EPOCH;
-		const timestampNumber = Number(timestamp)
+		const timestampNumber = Number(timestamp);
 		const workerId = (snowflake & 0x3e_00_00n) >> 17n;
 		const processId = (snowflake & 0x1_f0_00n) >> 12n;
 		const increment = snowflake & 0xf_ffn;
@@ -34,6 +34,6 @@ export function deconstruct(input: string): Snowflake | undefined {
 			increment: Number(increment),
 			$raw: { timestamp, workerId, processId, increment },
 		};
-	// eslint-disable-next-line no-empty
+		// eslint-disable-next-line no-empty
 	} catch {}
 }
