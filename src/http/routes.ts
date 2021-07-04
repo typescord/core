7d3801854c587d5cdbce73f2ee5df51d64ee89c9
Mestery <mestery@pm.me>
2021-07-04 21:57:44 +0200

    fix: routes urls

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,5 +4,5 @@ import { StaticRoute } from './routing';
 // TODO: simplify this when TypeScript will CORRECTLY infer some types (https://github.com/microsoft/TypeScript/issues/10571)
 
 // Gateway
-export const gateway = '/gateway' as StaticRoute<{ get: { r: d.RESTGetAPIGatewayResult } }>;
-export const gatewayBot = '/gateway/bot' as StaticRoute<{ get: { r: d.RESTGetAPIGatewayBotResult } }>;
+export const gateway = 'gateway' as StaticRoute<{ get: { r: d.RESTGetAPIGatewayResult } }>;
+export const gatewayBot = 'gateway/bot' as StaticRoute<{ get: { r: d.RESTGetAPIGatewayBotResult } }>;
